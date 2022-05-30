a4a7110144e7fcb621e4df4623a9c708fa6e0ffc
Major <90963895+Majorfi@users.noreply.github.com>
2022-05-30 15:15:23 +0200

    feat: add meta API (#288)
    * feat: build api
    
    * fix: linting and vercel path
    
    * fix: prettier ignore

---
+++ api/pages/api/strategies/[chainID]/[name].tsx
@@ -0,0 +1,48 @@
+import	fs										from	'fs';
+import	path									from	'path';
+import	type {NextApiRequest, NextApiResponse}	from	'next';
+
+const	dir = '../data/strategies';
+function readFiles(chainID: number, name: string, localization: string): unknown {
+	let		hasData = false;
+	let		data = {};
+	let		file = '';
+	try {
+		file = fs.readFileSync(path.resolve(`${dir}/${chainID}`, `${name}.json`), 'utf8');
+	} catch(e) {
+		return null;
+	}
+	const	jsonFileContent = JSON.parse(file);
+	if (localization === 'all') {
+		data = jsonFileContent;
+		hasData = true;
+	} else {
+		if (jsonFileContent.localization[localization]) {
+			jsonFileContent.description = jsonFileContent.localization[localization].description;
+			jsonFileContent.name = jsonFileContent.localization[localization].name;
+			delete jsonFileContent.localization;
+			data = jsonFileContent;
+			hasData = true;
+		}
+	}
+	if (!hasData) {
+		return null;
+	}
+	return data;
+}
+
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+	const	name = req.query.name;
+	const	chainID = Number(req.query.chainID || 1);
+	const	localization = req.query.loc || 'en';
+	if (!name) {
+		res.status(404).json({error: 'No strategy name provided'});
+		return;
+	}
+	const	data = readFiles(chainID, name as string, localization as string);
+	if (!data) {
+		res.status(404).json({error: 'Invalid strategy name'});
+		return;
+	}
+	res.status(200).json(data);
+};
