d9d0c25c1ffb2d78c35d4b092d476b6e9351fdfc
Major <90963895+Majorfi@users.noreply.github.com>
2022-05-30 18:10:25 +0200

    feat: add tokenList support (#289)
    * feat: add tokenList support
    
    * lint: fixing linting errors

---
+++ api/pages/api/tokens/list.tsx
@@ -0,0 +1,27 @@
+import	fs										from	'fs';
+import	path									from	'path';
+import	type {NextApiRequest, NextApiResponse}	from	'next';
+
+const	dir = '../data/tokens';
+function readFiles(): unknown {
+	let		data = {};
+	let		file = '';
+	try {
+		file = fs.readFileSync(path.resolve(`${dir}`, 'tokenList.json'), 'utf8');
+	} catch(e) {
+		return null;
+	}
+	const	jsonFileContent = JSON.parse(file);
+	delete jsonFileContent.$schema;
+	data = jsonFileContent;
+	return data;
+}
+
+export default (_req: NextApiRequest, res: NextApiResponse): void => {
+	const	data = readFiles();
+	if (!data) {
+		res.status(404).json({error: 'Invalid token address'});
+		return;
+	}
+	res.status(200).json(data);
+};
