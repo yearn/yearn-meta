6fc5dbc5feac0af0b1328dc4f7063bac59d6ffbd
Majorfi <major-tom.eth@pm.me>
2022-05-31 11:15:29 +0200

    fix: add cors and fix icons uri

--- api/pages/api/tokens/list.tsx
+++ api/pages/api/tokens/list.tsx
@@ -17,10 +17,21 @@ function readFiles(): unknown {
 	return data;
 }
 
-export default (_req: NextApiRequest, res: NextApiResponse): void => {
+export default (req: NextApiRequest, res: NextApiResponse): void => {
 	const	data = readFiles();
 	if (!data) {
 		res.status(404).json({error: 'Invalid token address'});
+		return;
+	}
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	);
+	if (req.method === 'OPTIONS') {
+		res.status(200).end();
 		return;
 	}
 	res.status(200).json(data);
