0adcd688cdb657c9db1c39435b1d1b8ac9e04432
Majorfi <major-tom.eth@pm.me>
2022-06-02 10:49:26 +0200

    fix: duplicate cors instruction

--- api/lib/allowCors.tsx
+++ api/lib/allowCors.tsx
@@ -3,9 +3,8 @@ import	type {NextApiRequest, NextApiResponse}	from	'next';
 type	TFunction = (req: NextApiRequest, res: NextApiResponse) => Promise<unknown>;
 const	allowCors = (fn: TFunction): unknown => async (req: NextApiRequest, res: NextApiResponse): Promise<unknown> => {
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
-	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-	res.setHeader('Access-Control-Allow-Origin', req.headers.origin as string);
+	res.setHeader('Access-Control-Allow-Origin', req?.headers?.origin || '*');
 	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
